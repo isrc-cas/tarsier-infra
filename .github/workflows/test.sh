847aaf1c17142657d01fac7c37f400339aa6d00b
Wenyuan Jiang <1951510@tongji.edu.cn>
2022-08-05 00:39:31 +0800

    Add Account jwy and modified CI script to support duplicated user detection. (#46)
    * Added a cat named jwy.
    
    * Updated test.sh to print duplicated users.
    
    * changed tab to space.
    
    * Updated CI bash script to detect duplicated users.

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -4,6 +4,18 @@ set -e
 echo "Parsing user_map.json"
 users="$(jq -r .userMap[][] user_map.json)"
 
+echo "Checking duplicated username..."
+dupusers="$(jq -r .userMap[][] user_map.json | sort | uniq -c | grep -v "^[[:space:]]*1 .*" | sed 's| *[0-9]*||g')"
+DUPLICATED_USERS=0
+for user in ${dupusers[@]}; do
+    echo "Found duplicated user: $user"
+	DUPLICATED_USERS="$((DUPLICATED_USERS + 1))"
+done
+if [[ ${DUPLICATED_USERS} -gt 0 ]]; then
+    echo "Found ${DUPLICATED_USERS} duplicated users."
+    exit "${DUPLICATED_USERS}"
+fi
+
 MISSING_USERS=0
 for user in ${users[@]}; do
     echo "Fetching keys for $user..."
