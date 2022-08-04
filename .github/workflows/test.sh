bbfeaec409dd1f2143b37e23462cec6b2ceaf345
jwyjohn <jwyjohn@foxmail.com>
2022-08-04 22:10:07 +0800

    Updated test.sh to print duplicated users.

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -4,6 +4,10 @@ set -e
 echo "Parsing user_map.json"
 users="$(jq -r .userMap[][] user_map.json)"
 
+echo "Checking duplicated username..."
+dupusers="$(jq -r .userMap[][] user_map.json | sort | uniq -c | grep -v "^[[:space:]]*1 .*" | sed 's| *[0-9]*||g')"
+echo "Duplicated users: $dupusers"
+
 MISSING_USERS=0
 for user in ${users[@]}; do
     echo "Fetching keys for $user..."
